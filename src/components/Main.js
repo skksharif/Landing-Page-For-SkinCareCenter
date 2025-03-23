1ba6898165fdc9be30d5dc90e6224012ab283f37
skksharif <khasimsharif12@gmail.com>
2025-03-23 23:17:54 +0530

    Final 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,25 +4,28 @@ import "./Main.css";
 export default function Main() {
   return (
     <main className="main-container">
-      <section className="section-image">
-        <img
-          src="./images/main.png  "
-          alt="Dr. Suja Skin Care"
-          className="main-image"
-        />
-      </section>
-      <section className="section-content">
-        <h1 className="heading">
-          <span id="pink">Dr.Kotla Sujalalitha </span>
-          <span id="green">MBBS,MD(DVL)</span>
-        </h1>
-        <p className="paragraph">
-          It is run by Dermatologist, we combine expertise, technology, and care to
-          bring out the best in your skin. From medical dermatology to cosmetic
-          procedures, we are dedicated to helping you achieve radiant, healthy
-          skin. Let your beauty shine through!
-        </p>
-        <button className="button">Contact Now</button>
+      <section className="inner-main">
+        <section className="section-image">
+          <img
+            src="./images/main.png"
+            alt="Dr. Suja Skin Care"
+            className="main-image"
+          />
+        </section>
+        <section className="section-content">
+          <h1 className="heading">
+            <span id="pink">Dr. Kotla Sujalalitha</span>
+            <br />
+            <span id="green">MBBS, MD (DVL)</span>
+          </h1>
+          <p className="paragraph">
+            Run by an expert dermatologist, we blend cutting-edge technology
+            with personalized care to enhance your skin’s natural beauty. From
+            medical dermatology to advanced cosmetic treatments, we’re here to
+            help you achieve radiant, healthy skin.
+          </p>
+          <button className="button">Contact Now</button>
+        </section>
       </section>
     </main>
   );
